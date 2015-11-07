d63d08d0aaaf20dea6bd5d27acd1871f38b0f4f7
Reshma Vasudevan <reshma_vasudevan@yahoo.com>
2015-11-07 19:43:19 +0530

    first

---
+++ website/php/config.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+    // Mail Address and Name
+    'sender_email'   => 'mail@themeinjection.com',
+    'sender_name'    => 'Themeinjection',
+
+    // Mail Config
+    'mail_type'     => 'mail', // smtp or mail - mail is the php mail function
+    'smtp_server'   => 'smtp.server.com',
+    'smtp_port'     => '25',
+    'smtp_user'     => 'username',
+    'smtp_password' => 'xxxxxxxxxxx',
+
+    // Mail Subjects
+    'contact_form_subject' => 'New message form website',
+    'newsletter_form_subject' => 'New message form website',
+    'inquiry_form_subject' => 'New inquiry form website',
+    'inquiry_autoresponder_subject' => 'Thanks for your inquiry',
+
+    // Mailchimp
+    'mailchimp_support' => false, // ture is activated
+    'mailchimp_api_key' => 'xxxxxx-your-api-key-xxxxxx',
+    'mailchimp_list_id' => 'xxx-list-id-xxx',
+);
